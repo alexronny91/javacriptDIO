4809f1288c787ce314631834552de56e25afb816
Alex Ronny <alexronny91@gmail.com>
2021-02-24 13:38:29 -0300

    Adionado exemplo de funções em js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
-var d = new Date()
-//alert(d.getMonth()+1)
-alert(d.getMinutes())
-alert(d.getDay())
-alert(d.getMinutes())
+function soma(n1, n2){
+    return (n1 +n2)
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+//var validar (Escopo Global)
+function validaIdade(idade){
+    var validar //(Escopo Local)
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar
+}
+
+var idade = prompt("Qual a sua idade?")
+console.log(validaIdade(idade))
+
+// alert(soma(5, 10))
+// alert(setReplace("Vai Japão", "Japão", "Brasil"))
+
+// var d = new Date()
+// //alert(d.getMonth()+1)
+// alert(d.getMinutes())
+// alert(d.getDay())
+// alert(d.getMinutes())
 
 // var count
 // for(count=0; count <= 5; count++){
