95adf619bf95dd503c15f6962704a77d542d8583
Alex Ronny <alexronny91@gmail.com>
2021-02-24 12:39:39 -0300

    Adicionado exemplos de condições, laços de repetição e date em js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,55 @@
-var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
-console.log(frutas)
-alert(frutas[1].nome)
+var d = new Date()
+//alert(d.getMonth()+1)
+alert(d.getMinutes())
+alert(d.getDay())
+alert(d.getMinutes())
 
-/*
-var fruta = {nome:"maça", cor:"vermelha"}
-console.log(fruta.nome)
-alert(fruta.cor)
-*/
+// var count
+// for(count=0; count <= 5; count++){
+//     alert(count)
+// }
 
-// var lista = ["maça", "perâ", "laranja"]
-// lista.push("uva")
-// //console.log(lista)
-// //console.log(lista.length)
-// //console.log(lista.reverse())
-// console.log(lista.toString())
-// console.log(lista.join(" - "))
+// var count = 0
+// while (count <= 5){
+//     console.log(count)
+//     alert(count)
+//     //count = count + 1
+//     count++
+// }
 
-// /*
-// var nome = "Alex Ronny"
-// var n1 = 5
-// var n2 = 3
-// var frase = "Japão é o melhor time do mundo"
-// //alert(nome + " tem " + idade + " anos")
-// //alert(idade + idade2)
-// console.log(nome)
-// console.log(n1 * n2)
-// console.log(frase.toLowerCase())
-// //alert(frase.replace("Japão","Brasil"))
-// */
+// var idade = prompt("Qual a sua idade?")
+// //var idade = 18
+// if(idade >= 18){
+//     alert("Maior de idade")
+// }else{
+//     alert("Menor de idade")
+// }
+
+// var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+// console.log(frutas)
+// alert(frutas[1].nome)
+
+// var fruta = {nome:"maça", cor:"vermelha"}
+// console.log(fruta.nome)
+// // alert(fruta.cor)
+
+// // var lista = ["maça", "perâ", "laranja"]
+// // lista.push("uva")
+// // //console.log(lista)
+// // //console.log(lista.length)
+// // //console.log(lista.reverse())
+// // console.log(lista.toString())
+// // console.log(lista.join(" - "))
+
+// // /*
+// // var nome = "Alex Ronny"
+// // var n1 = 5
+// // var n2 = 3
+// // var frase = "Japão é o melhor time do mundo"
+// // //alert(nome + " tem " + idade + " anos")
+// // //alert(idade + idade2)
+// // console.log(nome)
+// // console.log(n1 * n2)
+// // console.log(frase.toLowerCase())
+// // //alert(frase.replace("Japão","Brasil"))
+// // */
